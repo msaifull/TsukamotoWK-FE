9f1fc716edcfe8844e6a6e11b68f2297f430fd7a
msaifull <saifulguardian92@gmail.com>
2022-07-04 06:14:12 +0700

    sesi-16up

--- src/configs/index.js
+++ src/configs/index.js
@@ -3,8 +3,12 @@
 // dotenv.config();
 
 let config = {
-  api_host: "http://localhost:4000",
-  api_image: "http://localhost:4000/uploads",
+  api_host: "https://server-semina-v3.herokuapp.com/",
+  api_image: "https://server-semina-v3.herokuapp.com/uploads/",
 };
+// let config = {
+//   api_host: "http://localhost:4000",
+//   api_image: "http://localhost:4000/uploads",
+// };
 
 export { config };
